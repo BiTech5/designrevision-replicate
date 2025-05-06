29bd4ffee0a1cb9489288468b64131254dfc0a37
Bikram <bikikc7@gmail.com>
2025-05-06 17:53:03 +0545

    Adding footer component in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Sidebar from "./components/layout/sidebar"
 import Navbar from "./components/layout/navbar"
 import Table from "./pages/table"
+import Footer from "./components/layout/footer"
 function App() {
   return (
     <div className="flex min-h-screen bg-gray-50">
@@ -10,6 +11,7 @@ function App() {
         <div className="p-4 flex-1  bg-gray-100">
           <Table />
         </div>
+        <Footer />
       </div>
     </div>
   )
