1de4b099d991b818f28e46850014a46f190e735e
Bikram <bikikc7@gmail.com>
2025-05-06 18:09:38 +0545

    Updating Error page

--- src/components/Error/404.tsx
+++ src/components/Error/404.tsx
@@ -3,13 +3,14 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
     return (
         <div className="h-screen flex flex-col items-center justify-center bg-gray-100 text-center">
-            <h1 className="text-6xl font-bold text-blue-500">404</h1>
-            <p className="text-xl text-gray-600 mt-4">Oops! The page you're looking for doesn't exist.</p>
+            <h1 className="text-6xl font-bold text-gray-400">404</h1>
+            <p className="text-4xl  text-blue-950 mt-4">Something went wrong!</p>
+            <p className="text-sm text-gray-500">There was a problem on our end. Please try again later.</p>
             <Link
-                to="/"
-                className="mt-6 px-6 py-3 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600 transition"
+                to="/posts"
+                className="mt-6 px-6 py-2 bg-blue-500 text-white rounded-full shadow hover:bg-blue-600 transition text-sm" 
             >
-                Go Back Home
+                Go Back
             </Link>
         </div>
     );
