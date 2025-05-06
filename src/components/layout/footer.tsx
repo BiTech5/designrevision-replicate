4869fb7f51232d0bd1737a7438f1dd43811367e4
Bikram <bikikc7@gmail.com>
2025-05-06 17:48:42 +0545

    making footer

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+    return (
+        <>
+            <footer className="bg-white shadow-lg h-12">
+                <ul className="flex flex-cols items-center px-4 text-blue-400 text-sm">
+                    <li className="mr-6 cursor-pointer">Home</li>
+                    <li className="mr-6 cursor-pointer">Services</li>
+                    <li className="mr-6 cursor-pointer">About</li>
+                    <li className="mr-6 cursor-pointer">Products</li>
+                    <li className="mr-6 cursor-pointer">Blog</li>
+                </ul>
+            </footer>
+        </>
+    )
+}
+export default Footer;
