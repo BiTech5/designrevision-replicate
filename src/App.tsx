2cfa8ea3e9db7c54751f16eb5add5305fb8ceb51
Bikram <bikikc7@gmail.com>
2025-05-06 18:00:31 +0545

    Making routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import Sidebar from "./components/layout/sidebar"
 import Navbar from "./components/layout/navbar"
 import Table from "./pages/table"
 import Footer from "./components/layout/footer"
+import { Routes, Route } from "react-router-dom"
+import Blog from "./pages/blog_posts"
+import NotFound from "./components/Error/404"
 function App() {
   return (
     <div className="flex min-h-screen bg-gray-50">
@@ -9,7 +12,11 @@ function App() {
       <div className="flex-1 ml-52 flex flex-col">
         <Navbar />
         <div className="p-4 flex-1  bg-gray-100">
-          <Table />
+          <Routes>
+            <Route path="/posts" element={<Blog />} />
+            <Route path="/tables" element={<Table />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
         <Footer />
       </div>
