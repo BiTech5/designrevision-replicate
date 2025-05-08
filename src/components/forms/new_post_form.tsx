dd138905d6ca1d6c44ddcdd212e6dc31dbc023ac
Bikram <bikikc7@gmail.com>
2025-05-08 14:03:21 +0545

    few update

--- src/components/forms/new_post_form.tsx
+++ src/components/forms/new_post_form.tsx
@@ -6,10 +6,6 @@ const PostForm = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
 
-  const handleSubmit = () => {
-    console.log({ title, content });
-  };
-
   return (
     <div className="max-w-4xl mx-auto">
       <div className="bg-white rounded-lg shadow border border-gray-200">
